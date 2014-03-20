df07690f606ec4525196523567e93d40f69fdbe7
Diogo Melo <diogro@gmail.com>
2014-03-20 11:24:04 +0000

    trying out some mixed models. not quite there...

--- SASreplica.R
+++ SASreplica.R
@@ -24,3 +24,17 @@ mouse.data = cbind(arrange(mouse.phen, ID), arrange(mouse.gen, ID)[,-1])
 
 m.data = melt(mouse.data, id.vars = names(mouse.data)[c(1:27, 41:133)])
 
+null.formula = "value ~ variable * SEX + (1|FAMILY)"
+locus = 37
+genotype.formula = paste(null.formula, 
+                         paste(paste(c('A', 'D', 'I'), 
+                                     locus, sep = ''), collapse = ' + '), 
+                         sep = ' + ')
+mouse.model.no.gen = lmer(as.formula(null.formula), 
+                          data = m.data, 
+                          REML=FALSE)
+mouse.model = lmer(as.formula(genotype.formula), 
+                   data = m.data, 
+                   REML=FALSE)
+anova(mouse.model.no.gen, mouse.model)
+
