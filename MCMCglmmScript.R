50f7b039b102d3cbfad52235041d2d29ad0f7cdb
Diogo Melo <diogro@gmail.com>
2014-03-20 20:32:20 +0000

    MCMCglmm model start

--- MCMCglmmScript.R
+++ MCMCglmmScript.R
@@ -5,3 +5,28 @@ library(lme4)
 library(ggplot2)
 
 source('read.mouse.data.R')
+
+num.traits = 7
+
+values = paste("cbind(", 
+               paste(paste("grow", 
+                           c(12, 23, 34, 45, 56, 67, 78), 
+                           sep = ''), collapse = ', '),
+               ")", sep = '')
+fixed.effects = "trait:SEX - 1"
+
+null.formula = paste(values, fixed.effects, sep = ' ~ ')
+
+
+prior = list(R = list(V = diag(7), n = 20),
+             G = list(G1 = list(V = diag(7), n = 20)))
+mcmc.mouse.model = MCMCglmm( as.formula(null.formula),
+                             random = ~us(trait:FAMILY),
+                             data = mouse.data,
+                             rcov = ~us(trait):units,
+                             family = rep("gaussian", 7),
+                             prior = prior,
+                             verbose = TRUE)
+
+Ematrix = apply(array(mcmc.mouse.model$VCV, dim = c(1000, num.traits, num.traits)), 2:3, mean)
+
