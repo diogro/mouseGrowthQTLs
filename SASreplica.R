ff8a82ed78acf0d1d85951b4d071a78fc868f849
Diogo Melo <diogro@gmail.com>
2014-03-20 16:05:39 +0000

    weird bug in reading files...

--- SASreplica.R
+++ SASreplica.R
@@ -2,8 +2,9 @@ library(gdata)
 library(dplyr)
 library(reshape2)
 library(lme4)
+library(ggplot2)
 
-raw.mouse.phen = read.xls("./F3Phenotypes_further corrected family data_corrected litter sizes.xls")
+raw.mouse.phen = read.xls("./F3Phenotypes_further corrected family data_corrected litter sizes.xls")[1:37]
 names(raw.mouse.phen) = gsub('SexAN', 'SEX', names(raw.mouse.phen))
 
 raw.mouse.phen = mutate(raw.mouse.phen,
@@ -22,19 +23,25 @@ mouse.gen = raw.mouse.gen[raw.mouse.gen$ID %in% mouse.phen$ID,]
 mouse.phen = mouse.phen[mouse.phen$ID %in% mouse.gen$ID,]
 mouse.data = cbind(arrange(mouse.phen, ID), arrange(mouse.gen, ID)[,-1])
 
-m.data = melt(mouse.data, id.vars = names(mouse.data)[c(1:27, 41:133)])
+m.data = melt(mouse.data, id.vars = names(mouse.data)[c(1:27, 35:127)])
+names(m.data) = gsub('variable', 'trait', names(m.data))
 
-null.formula = "value ~ variable * SEX + (1|FAMILY)"
-locus = 37
-genotype.formula = paste(null.formula, 
-                         paste(paste(c('A', 'D', 'I'), 
-                                     locus, sep = ''), collapse = ' + '), 
-                         sep = ' + ')
+ggplot(m.data, aes(trait, value, color =SEX)) + geom_boxplot()
+
+null.formula = "value ~ 1 + trait * SEX + (1 + trait | FAMILY)"
 mouse.model.no.gen = lmer(as.formula(null.formula), 
                           data = m.data, 
                           REML=FALSE)
+
+locus = 1
+genotype.formula = paste(null.formula, 
+                         paste(paste('trait*', c('A', 'D', 'I'), 
+                                     locus, sep = ''), collapse = ' + '), 
+                         sep = ' + ')
 mouse.model = lmer(as.formula(genotype.formula), 
                    data = m.data, 
                    REML=FALSE)
 anova(mouse.model.no.gen, mouse.model)
+
+
 
