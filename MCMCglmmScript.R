493e9cbf72b4507eee2948ce86cb671ced92c57c
Diogo Melo <diogro@gmail.com>
2014-03-22 17:51:17 +0000

    MCMC all loci tests

--- MCMCglmmScript.R
+++ MCMCglmmScript.R
@@ -6,11 +6,13 @@ library(ggplot2)
 
 source('read.mouse.data.R')
 
+pedigree = select(mouse.data, ID, Dam, Sire)
+
 num.traits = 7
 
 values = paste("cbind(", 
                paste(paste("grow", 
-                           c(12, 23, 34, 45, 56, 67, 78), 
+                           paste(1:7, 2:8, sep = ''), 
                            sep = ''), collapse = ', '),
                ")", sep = '')
 fixed.effects = "trait:SEX - 1"
@@ -29,4 +31,23 @@ mcmc.mouse.model = MCMCglmm( as.formula(null.formula),
 
 G.mcmc = apply(array(mcmc.mouse.model$VCV, dim = c(1000, num.traits, num.traits)), 2:3, mean)
 
-pedigree = select(mouse.data, ID, Dam, Sire)
+runSingleLocusMCMCModel <- function(locus, null.formula){
+  genotype.formula = paste(null.formula, 
+                           paste(paste('trait:', c('A', 'D', 'I'),
+                                       locus, sep = ''), collapse = ' + '),
+                           sep = ' + ')
+  prior = list(R = list(V = diag(7), n = 0.002),
+               G = list(G1 = list(V = diag(7) * 0.02, n = 8)))
+  mcmc.mouse.model = MCMCglmm( as.formula(genotype.formula),
+                               random = ~us(trait):FAMILY,
+                               data = mouse.data,
+                               rcov = ~us(trait):units,
+                               family = rep("gaussian", 7),
+                               prior = prior,
+                               verbose = FALSE)
+  return(mcmc.mouse.model)
+}
+
+# all.loci.MCMC = alply(1:31, 1, runSingleLocusMCMCModel, null.formula, .progress='text')
+# save(all.loci.MCMC, file= 'mouse.cromossome1.MCMC.Rdata')
+load("./mouse.cromossome1.MCMC.Rdata")
