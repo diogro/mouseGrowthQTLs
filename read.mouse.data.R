b4c2b8fca845308660d53150527272b443533856
diogro <diogro@gmail.com>
2016-04-27 13:09:07 -0300

    better data read

--- read.mouse.data.R
+++ read.mouse.data.R
@@ -1,10 +1,11 @@
-library(gdata)
-library(plyr)
-library(dplyr)
-library(reshape2)
+if(!require(gdata)) {install.packages("gdata"); library(gdata)}
+if(!require(plyr)) {install.packages("plyr"); library(plyr)}
+if(!require(dplyr)) {install.packages("dplyr"); library(dplyr)}
+if(!require(reshape2)) {install.packages("reshape2"); library(reshape2)}
+if(!require(readr)) {install.packages("readr"); library(readr)}
 
-raw.mouse_phen = read.csv("./data/F3phenotypes_uncorrected.csv", as.is = T)
-raw.mouse_phen = select(raw.mouse_phen, c(ID, FATPAD:PAIR))
+raw.mouse_phen = read_csv("./data/F3phenotypes_uncorrected.csv")
+raw.mouse_phen = tbl_df(select(raw.mouse_phen, c(ID, FATPAD:LIVER, WEEK1:WEEK10)))
 
 raw.mouse_phen = mutate(raw.mouse_phen,
                           grow12 = WEEK2 - WEEK1,
@@ -15,19 +16,17 @@ raw.mouse_phen = mutate(raw.mouse_phen,
                           grow67 = WEEK7 - WEEK6,
                           grow78 = WEEK8 - WEEK7)
 
-raw.mouse_meta = read.csv("./data/F3Phenotypes_further corrected family data_corrected litter sizes.csv", as.is = T)
+raw.mouse_meta = read_csv("./data/F3Phenotypes_further corrected family data_corrected litter sizes.csv")
 names(raw.mouse_meta) = gsub('SexAN', 'SEX', names(raw.mouse_meta))
 raw.mouse_meta = select(raw.mouse_meta, ID:COHORT)
+mouse_phen = inner_join(raw.mouse_meta, raw.mouse_phen, by = "ID")
 
-raw.mouse_meta = raw.mouse_meta[raw.mouse_meta$ID %in% raw.mouse_phen$ID,]
-raw.mouse_phen = raw.mouse_phen[raw.mouse_phen$ID %in% raw.mouse_meta$ID,]
-
-mouse_phen = data.frame(arrange(raw.mouse_meta, ID), arrange(raw.mouse_phen, ID)[,-1])
-raw.mouse_gen = llply(paste0("./data/genotypes/chrom", 1:19, ".csv"), read.csv, as.is = TRUE)
+raw.mouse_gen = llply(paste0("./data/genotypes/chrom", 1:19, ".csv"), read_csv)
 names(raw.mouse_gen) = paste0("chrom", 1:19)
-mouse_gen = llply(raw.mouse_gen, function(x) x[x$ID %in% mouse_phen$ID,])
+mouse_gen = llply(raw.mouse_gen, function(x) semi_join(x, mouse_phen, by = "ID"))
 mouse_gen = llply(mouse_gen, function(x) arrange(x, ID))
-mouse_phen = mouse_phen[mouse_phen$ID %in% mouse_gen[[1]]$ID,]
+
+mouse_phen = semi_join(mouse_phen, mouse_gen[[1]], by = "ID")
 mouse_phen = arrange(mouse_phen, ID)
 
 rm(list = ls(pattern='raw'))
@@ -51,4 +50,4 @@ exclude = c(dim(m.data.std)[2]-1, dim(m.data.std)[2])
 cast_formula = paste(paste(names(m.data.std[,-exclude]), collapse = " + "),
                      'variable',
                      sep = " ~ ")
-mouse_phen_std = dcast(m.data.std, as.formula(cast_formula))
+mouse_phen_std = tbl_df(dcast(m.data.std, as.formula(cast_formula)))
