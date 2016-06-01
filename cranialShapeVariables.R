7c6f9c52523c22e7eff6d834d40ea8b2ef884a12
Diogo Melo <diogro@gmail.com>
2016-06-01 14:57:38 +0100

    read landmarks and simulate additive effects

--- cranialShapeVariables.R
+++ cranialShapeVariables.R
@@ -13,3 +13,19 @@ tesselation = Aux$single.tessel.38
 
 wrapMarquez(OTU$ss, tesselation)
 
+raw_files = dir("data/Skull raw landmarks/")
+raw_files = paste0("./data/Skull raw landmarks/", raw_files)
+read_skulls = function(file){
+x = scan(file, character())
+char_lands = matrix(x[-c(1:3)], as.numeric(x[3]), 4, byrow = T)
+lands = matrix(as.numeric(char_lands[,-1]), as.numeric(x[3]), 3)
+colnames(lands) = c("x", "y", "z")
+rownames(lands) = char_lands[,1]
+lands}
+landmarks = aperm(laply(raw_files, read_skulls), c(2, 3, 1))
+
+library(geomorph)
+raw_files[191]
+skull_gpa = gpagen(landmarks)
+mean_spec = findMeanSpec(skull_gpa$coords)
+read.ply("./data/Multivariate/Data from Klingenberg/Ply files/1381.PLY")
--- qtlMapping/simulated_additive_effect.R
+++ qtlMapping/simulated_additive_effect.R
@@ -2,16 +2,35 @@ setwd("~/projects/mouse-qtls/")
 source("~/projects/mouse-qtls/read_mouse_data.R")
 source("~/projects/mouse-qtls/OAuth_lem_server.R")
 
+data = data.frame(z = rnorm(1000, 1, 10), 
+                  marker = sample(c(-1, 0, 1), 1000, replace = TRUE))
+
 a = 2.2
 markerVar = function(x){
     data$za = data$z + data$marker * a/2
     V_pa = sum((data$za - mean(data$za))^2)/(1000 - 1)
     gen_means = tapply(data$za, data$marker, mean)
-    gen_n = table(data$marker)
-    var_marker = gen_n %*% (gen_means - mean(data$za))^2 / (1000 - 3)
+    gen_n = table(data$marker) / (1000 - 3)
+    var_marker = gen_n %*% (gen_means - mean(data$za))^2
     (var_marker / V_pa)[1]
 }
+markerVar(1)
 
-simulated_markers
+V_p = sum((data$za - mean(data$za))^2)/(1000 - 1)
+gen_means = tapply(data$za, data$marker, mean)
+gen_n = table(data$marker)/1000
+V_a = gen_n %*% (gen_means - mean(data$za))^2
+(var_marker / V_pa)[1]
 
+eff_var = 0.1
+
+2*(eff_var * V_p / (1-eff_var) - V_a)/(gen_n[3] - gen_n[1])
+
+sim_chrom_number = 1
+area_data = inner_join(area_phen_std, simulated_markers[[sim_chrom_number]], by = "ID")
+
+current_loci = 1
+names(area_data)
+
+sim_data = dplyr::select(area_data, area1:area7, matches("A1$"))
 
