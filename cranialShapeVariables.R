41a55e039b5f916714a60ae8e7eeabea13184c5a
Diogo Melo <diogro@gmail.com>
2016-06-01 17:41:34 +0100

    shape animations

--- cranialShapeVariables.R
+++ cranialShapeVariables.R
@@ -1,31 +1,61 @@
 library(evolqg)
+library(plyr)
 library(doMC)
 registerDoMC(4)
 
-source("./ggshape.R")
-source("./splines.R")
-
-load("./Aux.RData")
-load("~/Dropbox/labbio/data/cov_bayes_data/Rdatas/monkeys.RData")
-
-OTU = main.data$Homo_sapiens
-tesselation = Aux$single.tessel.38
-
-wrapMarquez(OTU$ss, tesselation)
+# source("./ggshape.R")
+# source("./splines.R")
+#
+# load("./Aux.RData")
+# load("~/Dropbox/labbio/data/cov_bayes_data/Rdatas/monkeys.RData")
+#
+# OTU = main.data$Homo_sapiens
+# tesselation = Aux$single.tessel.38
+#
+# wrapMarquez(OTU$ss, tesselation)
 
 raw_files = dir("data/Skull raw landmarks/")
 raw_files = paste0("./data/Skull raw landmarks/", raw_files)
 read_skulls = function(file){
-x = scan(file, character())
-char_lands = matrix(x[-c(1:3)], as.numeric(x[3]), 4, byrow = T)
-lands = matrix(as.numeric(char_lands[,-1]), as.numeric(x[3]), 3)
-colnames(lands) = c("x", "y", "z")
-rownames(lands) = char_lands[,1]
-lands}
-landmarks = aperm(laply(raw_files, read_skulls), c(2, 3, 1))
+  x = scan(file, character())
+  char_lands = matrix(x[-c(1:3)], as.numeric(x[3]), 4, byrow = T)
+  lands = matrix(as.numeric(char_lands[,-1]), as.numeric(x[3]), 3)
+  colnames(lands) = c("x", "y", "z")
+  rownames(lands) = char_lands[,1]
+  lands
+}
+raw_landmarks = laply(raw_files, read_skulls)
+landmarks = aperm(raw_landmarks, c(2, 3, 1))
 
 library(geomorph)
-raw_files[191]
+
 skull_gpa = gpagen(landmarks)
+ref = mshape(skull_gpa$coords)
 mean_spec = findMeanSpec(skull_gpa$coords)
-read.ply("./data/Multivariate/Data from Klingenberg/Ply files/1381.PLY")
+raw_files[[mean_spec]]
+raw_mesh = read.ply("./data/landmarks/1381.PLY")
+mean_mesh = warpRefMesh(raw_mesh, landmarks[,,mean_spec], ref)
+
+pc.res <- prcomp(two.d.array(skull_gpa$coords))
+pcdata <- pc.res$x
+rotation <- pc.res$rotation
+k <- dim(skull_gpa$coords)[2] ; p <- dim(skull_gpa$coords)[1]
+pcaxis.quatiles <- quantile(pcdata[, 1], seq(0, 1, length.out = 10))
+pc1_loading = pcaxis.quatiles[[1]]
+
+pc1_shape <- function(pc1_loading) {
+  pc.vec <- rep(0, dim(pcdata)[2])
+  pc.vec[1] <- pc1_loading
+  target <- arrayspecs(as.matrix(pc.vec %*% (t(rotation))), p,k)[,,1] + ref
+  warpRefMesh(raw_mesh, landmarks[,,mean_spec], target)
+}
+pc1_shapes_list = llply(pcaxis.quatiles, pc1_shape, .progress = "text")
+plot_shape = function(shape){
+  open3d()
+  shade3d(rotate3d(shape, pi, 20, -8, 0))
+}
+
+for(i in 1:10){
+plot_shape(pc1_shapes_list[[i]])
+rgl.postscript(paste0("./animation/pc1_animation_", i, ".pdf"), fmt="pdf")
+}
