fce61030948116c3f677f394c2e7cd1d2f4e7e6e
Diogo Melo <diogro@gmail.com>
2016-06-06 12:42:14 +0100

    analytical effect size

--- qtlMapping/simulated_additive_effect.R
+++ qtlMapping/simulated_additive_effect.R
@@ -3,19 +3,16 @@ source("~/projects/mouse-qtls/read_mouse_data.R")
 source("~/projects/mouse-qtls/OAuth_lem_server.R")
 
 install_load("MCMCglmm","doMC")
-registerDoMC(80)
+registerDoMC(3)
 
-data = rnorm(1000, 1, 10)
-marker = sample(c(-1, 0, 1), 1000, replace = TRUE)
-a = 0
 
 makeSimData = function(data, marker, percent){
   a = findA(data, marker, percent)
-  data + marker * a/2
+  data + marker * a
 }
 markerVar = function(a, data, marker){
     data = mean(data) + residuals(lm(data ~ marker))
-    za = data + marker * a/2
+    za = data + marker * a
     n = length(data)
     V_pa = sum((za - mean(za))^2)/(n - 1)
     gen_means = tapply(za, marker, mean)
@@ -25,26 +22,21 @@ markerVar = function(a, data, marker){
 }
 findA = function(data, marker, percent){
   V_p = var(data)
-  step_size = (percent * V_p)
-  marker_var = 0
-  a = 0
-  while(marker_var <= percent){
-    a = a + step_size
-    marker_var = markerVar(a, data, marker)
-  }
-  step_size = abs(marker_var - percent)/10
-  while(marker_var >= percent){
-    a = a - step_size
-    marker_var = markerVar(a, data, marker)
-  }
-  return(a + step_size)
+  a = sqrt((2*percent*V_p)/(1 - percent))
+  return(a)
 }
+
+# data = rnorm(1000, 1, 10)
+# marker = sample(c(-1, 0, 1), 1000, replace = TRUE)
+# a = findA(data, marker, 0.01)
+# markerVar(a, data, marker)
 
 # sim_chrom_number = 1
 # locus = 1
 # effect_size = 0.01
 
 simulateDIC = function(locus, sim_chrom_number, effect_size){
+  print
   area_data = inner_join(area_phen_std, simulated_markers[[sim_chrom_number]], by = "ID")
 
   marker_column_A = data.frame(select(area_data, matches(paste0("_A", locus, "$"))))[,1]
@@ -53,6 +45,7 @@ simulateDIC = function(locus, sim_chrom_number, effect_size){
                                         makeSimData, marker_column_A, effect_size))) %>%
     mutate(A = marker_column_A, D = marker_column_D, FAMILY = area_phen_std$FAMILY)
 
+  print(paste(sim_chrom_number, locus))
   value = paste("cbind(", paste(area_traits, collapse = ', '), ")", sep = '')
 
   fixed_effects = "trait - 1"
@@ -95,7 +88,7 @@ DIC_list = vector("list", 20)
 names(DIC_list) = effect_size
 for(i in 1:20){
     ptm <- proc.time()
-    DIC_list[[i]] = ldply(1:1000, simulateDIC, i, effect_size[i], .parallel = TRUE)
+    DIC_list[[i]] = ldply(1:3, simulateDIC, i, effect_size[i], .parallel = TRUE)
     time = proc.time() - ptm
     dmSend(paste("Finished simulated chromossome", i, "in", round(time[3]/60, 2), "minutes." ), "diogro")
 }
