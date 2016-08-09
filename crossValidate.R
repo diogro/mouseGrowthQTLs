13bd266836344aabbb6935a540500e7ccf958919
diogro <diogro@gmail.com>
2016-08-09 14:21:50 -0300

    parallel cross validation

--- crossValidate.R
+++ crossValidate.R
@@ -3,19 +3,33 @@ source('stanFunctions.R')
 source('read_mouse_data.R')
 source('utils.R')
 
-Rdatas_folder = "~/gdrive/LGSM_project_Rdatas/"
-#Rdatas_folder = "./data/Rdatas/"
+#Rdatas_folder = "~/gdrive/LGSM_project_Rdatas/"
+Rdatas_folder = "./data/Rdatas/"
 
 rstan_options(auto_write = TRUE)
-chains = 10
 options(mc.cores = chains)
-
-growth_data = inner_join(growth_phen_std, markers, by = "ID")
-growth_data[growth_traits] = scale(growth_data[growth_traits])
-
-#growth_data = inner_join(growth_phen_std, markers, by = "ID")
-current_data = growth_data
-separateData = function(current_data, per_family = 1){
+getSqError = function(fit, test_data)
+{
+    getResidual = function(i){
+        residual = test_data$y -
+            (fit$w0[i,] + fit$beta_family[i,test_data$family,] +
+             t(fit$w_ad[i,,] %*% t(test_data$ad)) +
+             t(fit$w_dm[i,,] %*% t(test_data$dm)))
+        sum(residual^2)
+    }
+    n = nrow(fit[[1]])
+    aaply(1:n, 1, getResidual)
+}
+getMeanSqError = function(fit, test_data)
+{
+        residual = test_data$y -
+            (colMeans(fit$w0) + colMeans(fit$beta_family)[test_data$family,] +
+             t(colMeans(fit$w_ad) %*% t(test_data$ad)) +
+             t(colMeans(fit$w_dm) %*% t(test_data$dm)))
+        sum(residual^2)
+}
+separateData = function(current_data, per_family = 1)
+{
     current_data = ddply(current_data, .(FAMILY), function(x){
               n_ind = nrow(x)
               x$train = 0
@@ -24,36 +38,32 @@ separateData = function(current_data, per_family = 1){
     return(list(test = filter(current_data, train == 1),
                 train = filter(current_data, train == 0)))
 }
-x = separateData(growth_data)
-train_growth_data = x$train
-test_growth_data = x$test
 
-fit_SUR_HC = stan(file = './SUR_horseShoe.stan',
-                  data = getStanInputFullGenome(train_growth_data, growth_traits),
-                  chain=chains, iter = 200, warmup = 100)
-fit_SUR_HCp = stan(file = './SUR_horseShoePlus.stan',
-                   data = getStanInputFullGenome(train_growth_data, growth_traits),
-                   chain=chains, iter = 200, warmup = 100)
-fit_SUR_HAL = stan(file = './SUR_HAL.stan',
-                   data = getStanInputFullGenome(train_growth_data, growth_traits),
-                   chain=chains, iter = 200, warmup = 100)
-fit_SUR     = stan(file = './SUR.stan',
-                   data = getStanInputFullGenome(train_growth_data, growth_traits),
-                   chain=chains, iter = 200, warmup = 100)
+growth_data = inner_join(growth_phen_std, markers, by = "ID")
+growth_data[growth_traits] = scale(growth_data[growth_traits])
 
-test_data = getStanInputFullGenome(test_growth_data, growth_traits)
-fit = rstan::extract(fit_SUR_HC)
-getMeanSqError = function(fit, test_data){
-    getResidual = function(i){
-        residual = test_data$y -
-            (fit$w0[i,] + fit$beta_family[i,test_data$family,] + t(fit$w_ad[i,,] %*% t(test_data$ad)) + t(fit$w_dm[i,,] %*% t(test_data$dm)))
-        sum(residual^2)
-    }
-    n = nrow(fit[[1]])
-    aaply(1:n, 1, getResidual)
+crossValidate = function(current_data, trait_vector, iter, warmup){
+    x = separateData(current_data)
+    train_data = x$train
+    test_data = getStanInputFullGenome(x$test, trait_vector)
+
+    model_list = list(SUR = "./SUR.stan",
+                      HC = "./SUR_horseShoe.stan",
+                      HCp = "./SUR_horseShoePlus.stan",
+                      HAL = "./SUR_HAL.stan")
+    fits = llply(model_list,
+            function(model)
+            rstan::extract(stan(file = model,
+                data = getStanInputFullGenome(train_data, trait_vector),
+                chain=chains,
+                iter = iter,
+                warmup = warmup)), .parallel = TRUE)
+    ldply(fits, getMeanSqError, test_data)
 }
-fits = list(SUR = rstan::extract(fit_SUR),
-            HC  = rstan::extract(fit_SUR_HC),
-            HCp = rstan::extract(fit_SUR_HCp),
-            HAL = rstan::extract(fit_SUR_HAL))
-ldply(fits, function(fit) quantile(getMeanSqError(fit, test_data), c(0.25, 0.5, 0.975)))
+library(doMC)
+registerDoMC(10)
+chains = 1
+crossValidate(growth_data, growth_traits, iter = 200, warmup = 100)
+CV_msqe = llply(1:10, function(x) crossValidate(growth_data, growth_traits,
+                                                iter = 200, warmup = 100),
+                .parallel = TRUE)
