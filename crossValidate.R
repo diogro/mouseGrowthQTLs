1ac783e430ae0f2e56247e60e0a204d0f3d06268
diogro <diogro@gmail.com>
2016-08-23 09:58:02 -0300

    cross validation on necropsy

--- crossValidate.R
+++ crossValidate.R
@@ -24,8 +24,20 @@ getMeanSqError = function(fit, test_data)
 {
         residual = test_data$y -
             (colMeans(fit$w0) + colMeans(fit$beta_family)[test_data$family,] +
-             t(colMeans(fit$w_ad) %*% t(test_data$ad)) +
-             t(colMeans(fit$w_dm) %*% t(test_data$dm)))
+             t((colMeans(fit$w_ad)) %*% t(test_data$ad)) +
+             t((colMeans(fit$w_dm)) %*% t(test_data$dm)))
+        sum(residual^2)
+}
+getMeanSqErrorShrink = function(fit, test_data, cut_off = 0.5)
+{
+        ad_mask = colMeans(fit$shrink_ad)
+        ad_mask[] = as.numeric(colMeans(fit$shrink_ad)[] > cut_off)
+        dm_mask = colMeans(fit$shrink_dm)
+        dm_mask[] = as.numeric(colMeans(fit$shrink_dm)[] > cut_off)
+        residual = test_data$y -
+            (colMeans(fit$w0) + colMeans(fit$beta_family)[test_data$family,] +
+             t((ad_mask * colMeans(fit$w_ad)) %*% t(test_data$ad)) +
+             t((dm_mask * colMeans(fit$w_dm)) %*% t(test_data$dm)))
         sum(residual^2)
 }
 separateData = function(current_data, per_family = 1)
@@ -39,8 +51,8 @@ separateData = function(current_data, per_family = 1)
                 train = filter(current_data, train == 0)))
 }
 
-growth_data = inner_join(growth_phen_std, markers, by = "ID")
-growth_data[growth_traits] = scale(growth_data[growth_traits])
+necropsy_data = inner_join(necropsy_phen_std, markers, by = "ID")
+necropsy_data[necropsy_traits] = scale(necropsy_data[necropsy_traits])
 
 crossValidate = function(current_data, trait_vector, iter, warmup){
     x = separateData(current_data)
@@ -58,12 +70,25 @@ crossValidate = function(current_data, trait_vector, iter, warmup){
                 chain=chains,
                 iter = iter,
                 warmup = warmup)), .parallel = TRUE)
-    ldply(fits, getMeanSqError, test_data)
+    list(mse = ldply(fits, getMeanSqError, test_data),
+         fits = fits,
+         separated_data = x)
 }
 library(doMC)
 registerDoMC(10)
 chains = 1
-crossValidate(growth_data, growth_traits, iter = 200, warmup = 100)
-CV_msqe = llply(1:10, function(x) crossValidate(growth_data, growth_traits,
+crossValidate(necropsy_data, necropsy_traits, iter = 200, warmup = 100)
+CV_msqe = llply(1:10, function(x) crossValidate(necropsy_data, necropsy_traits,
                                                 iter = 200, warmup = 100),
                 .parallel = TRUE)
+write_rds(CV_msqe, paste0(Rdatas_folder, "cross_validation_necropsy.rds"))
+llply(CV_msqe, '[[', 1) %>% laply('[[',"V1") %>% aaply(1, function(x) x - min(x))
+
+
+fit = CV_msqe[[1]]$fits$HCp
+
+CV_shrink = laply(CV_msqe, function(part) aaply(seq(0.1, 0.4, 0.01), 1, function(x) getMeanSqErrorShrink(part$fits$HCp,
+               getStanInputFullGenome(part$separated_data$test,
+               necropsy_traits), x)))
+CV_rank = aaply(CV_shrink, 1, rank)
+colnames(CV_shrink) = colnames(CV_rank) = seq(0.1, 0.4, 0.01)
