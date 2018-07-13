b443a742c4f740d827ddb2d61ff54d24cc8e695d
diogro <diogro@gmail.com>
2018-07-13 17:46:34 -0300

    works!

--- growth_lme4qtl_conditionalEffectMapping.R
+++ growth_lme4qtl_conditionalEffectMapping.R
@@ -4,97 +4,72 @@ source('read_mouse_data.R')
 #Rdatas_folder = "~/gdrive/LGSM_project_Rdatas/"
 Rdatas_folder = "./Rdatas/"
 
-install_load("doMC", "lme4qtl", "qvalue")
-registerDoMC(2)
+ncores = 1
+nthreads = 8
+registerDoMC(ncores)
+options(mc.cores = ncores)
+setMKLthreads(nthreads)
 
 growth_data = inner_join(growth_phen_std,
                         growth_markers,
-                        by = "ID") %>%
-  gather(variable, value, growth12:growth78)
+                        by = "ID") 
 growth_data = mutate(growth_data, FAMILY = as.factor(FAMILY), variable = as.factor(variable))
-null_formula = "value ~ variable + (0 + variable|FAMILY)"
 
-makeMarkerList = function(pos) paste(paste('variable:chrom', pos[1],"_", c('A', 'D'), pos[2], sep = ''), collapse = ' + ')
 markerMatrix = ldply(1:19, function(x) data.frame(chrom = x, marker = 1:loci_per_chrom[[x]]))
+makeMarkerList = function(pos) paste(paste('chrom', pos[1],"_", c('A', 'D'), pos[2], sep = ''), collapse = ' + ')
 markerList = alply(markerMatrix, 1, makeMarkerList)
 markerPositions = cbind(markerMatrix, read_csv("./data/markers/marker_positions.csv")[,3])
 names(markerPositions)[3] = "cM"
 
-runIntervalModel <- function(marker_term, null_formula){
-    pos = na.omit(as.numeric(unlist(strsplit(unlist(as.character(marker_term)), "[^0-9]+"))))[3:2]
-    focal_cm = dplyr::filter(markerPositions, chrom == pos[1], marker == pos[2])$cM
-    possible_flank = filter(markerPositions, chrom == pos[1], abs(cM - focal_cm) > flank_dist)
-    flanking = NULL
-    for(line in 1:nrow(possible_flank)){
-        if(possible_flank[line,"cM"] > focal_cm){ flanking = rbind(flanking, possible_flank[line,1:2]); break }
-    }
-    if(line > 1 & line != nrow(possible_flank)) flanking = rbind(possible_flank[line-1, 1:2], flanking)
-    if(line == nrow(possible_flank)) flanking = rbind(possible_flank[line, 1:2], flanking)
-
-    flanking_formula = paste(null_formula, paste(alply(flanking, 1, makeMarkerList), collapse = " + "), sep = " + ")
-
-    flanking_model = relmatLmer(as.formula(flanking_formula),
-                                data = growth_data,
-                                REML = FALSE)
-
-
-    genotype_formula = paste(flanking_formula, marker_term, sep = ' + ')
-
-    focal_model = relmatLmer(as.formula(genotype_formula),
-                             data = growth_data,
-                             REML = FALSE)
-    test = anova(focal_model, flanking_model)
-
-    return(list(flanking = flanking_model,
-                focal    = focal_model, 
+current_trait = 2
+runInteractionModel <- function(current_trait, markers = 1:353){
+  trait_term = growth_traits[1:(current_trait-1)]
+  null_formula = paste(growth_traits[current_trait], " ~ (1|FAMILY)")
+  makeInteractionMarkerList = function(pos){
+    marker_term = paste('chrom', pos[1],"_", c('A', 'D'), pos[2], sep = '')
+    paste(apply(expand.grid(trait_term, marker_term), 1, paste, collapse = ":"), collapse = " + ")
+  }
+  interactionMarkerList = alply(markerMatrix, 1, makeInteractionMarkerList)
+  
+  runInteractionModelSingleMaker = function(current_marker){
+    direct_formula = paste(null_formula, markerList[current_marker], trait_term, sep = " + ")
+    direct_lmm = lmer(as.formula(direct_formula), data = growth_data, REML = FALSE)
+    conditional_formula = paste(direct_formula, interactionMarkerList[current_marker], sep = " + ")
+    conditional_lmm = lmer(growth23 ~ (1|FAMILY) + chrom1_A1 + chrom1_D1 + 
+                             growth12:chrom1_A1 + growth12:chrom1_D1, 
+                           data = growth_data, REML = FALSE)
+    test = anova(direct_lmm, conditional_lmm)
+    
+    return(list(direct_lmm = direct_lmm,
+                conditional_lmm = conditional_lmm, 
                 test = test,
-                G = VarCorr(focal_model)[[1]],
                 p.value = test$'Pr(>Chisq)'[2],
-                model_summary = summary(focal_model)))
+                model_summary = summary(conditional_lmm)))
+  }
+  llply(markers, runInteractionModelSingleMaker, .progress = "text")
 }
-flank_dist = 10
-model_file = paste0(Rdatas_folder, "growth_intervalMapping_", flank_dist, "cM.Rdata")
-# intervalMapping = llply(markerList, runIntervalModel, null_formula, .parallel = TRUE)
-# save(intervalMapping, file = model_file)
-load(model_file)
-
-x = intervalMapping[[10]]
-coef(x$model_summary)
-all_effectsInterval = ldply(intervalMapping,
-                            function(x){
-                                n_traits = length(growth_traits)
-                                coef_matrix =coef(x$model_summary) 
-                                n_coef = nrow(coef_matrix)
-                                ad = coef_matrix[(n_coef-2*n_traits + 1):(n_coef - n_traits),1]
-                                ad_se = coef_matrix[(n_coef-2*n_traits + 1):(n_coef - n_traits),2]
-                                dm = coef_matrix[(n_coef-n_traits + 1):n_coef,1]
-                                dm_se = coef_matrix[(n_coef-n_traits + 1):n_coef,2]
-                                data_frame(ad, ad_se, dm, dm_se)
-                            }, .id = NULL, .parallel = TRUE) %>% tbl_df %>% mutate(count = rep(seq(intervalMapping), each = num_growth_traits)) %>% select(count, everything())
-
-ldply(intervalMapping, function(x) -log10(x$p.value)) %>%
-    filter(chrom == 1) %>%
-    ggplot(aes(x =seq_along(V1), V1)) + geom_point() + geom_line()
-
-effect_file = paste0("./data/growth traits/growth_effectsInterval_", flank_dist, "cM.csv")
-write_csv(all_effectsInterval, effect_file)
+# conditional_gwas = vector("list", num_growth_traits-1)
+# names(conditional_gwas) = growth_traits[-1]
+# for(i in 1:(num_growth_traits-1)){
+#   print(growth_traits[i+1])
+#   conditional_gwas[[i]] = runInteractionModel(i+1)
+# }
+# save(conditional_gwas, file = "./Rdatas/growth_conditional_all_previous.Rdata")
+load("./Rdatas/growth_conditional_all_previous.Rdata")
 
 thresholds = read.csv("./data/F3_BONFERRONI_thresholds.csv")
 
-Pvalues = function(flank_dist, ...){
-    model_file = paste0(Rdatas_folder, "growth_intervalMapping_", flank_dist, "cM.Rdata")
-    load(model_file)
-    p.values = ldply(intervalMapping, function(x) x$p.value) 
+Pvalues = function(i, ...){
+    p.values = ldply(conditional_gwas[[i]], function(x) x$p.value) 
     p.values = rename(p.values, p_lrt = V1)
     p.values$significant = sapply(1:353, function(i) p.values$p_lrt[i] < thresholds[thresholds$chrom == markerMatrix[i, "chrom"], "p_value"])
-    p.values$flank_dist = flank_dist
     p.values$pos = markerPositions$cM
     p.values$snp = 1:353
+    p.values$trait = growth_traits[i+1]
+    p.values = cbind(markerMatrix, p.values)
     return(p.values)
 }
-p_values = ldply(c(5, 10, 15, 20), Pvalues)
-p_values$flank_dist_chr = factor(paste0("Flanking markers at ", p_values$flank_dist, "cM"), 
-                                 levels = paste0("Flanking markers at ", c(20, 15, 10, 5), "cM"))
+p_values = ldply(1:6, Pvalues)
 chrtable <- data.frame(table(p_values$chrom))
 chrtable$Var1 <- as.character(chrtable$Var1)
 chrtable <- chrtable[gtools:::mixedorder(chrtable$Var1), ]
@@ -130,17 +105,17 @@ length(unlist(x))
 significantMarkerMatrix = ldply(x, function(x) data.frame(marker = x), .id = "chrom") 
 significantMarkerMatrix$chrom = as.integer(as.character(significantMarkerMatrix$chrom))
 p_values_sig = inner_join(p_values, significantMarkerMatrix, by = c("chrom", "marker"))
-write_csv(significantMarkerMatrix, "./data/growth_significant_markers.csv")
-filter(p_values, significant == TRUE, chrom == 4, flank_dist == 10)
+#write_csv(significantMarkerMatrix, "./data/growth_significant_markers.csv")
 library(ggman)
 (p1 = ggplot(p_values, aes(snp, -log10(p_lrt), color = as.factor(chrom_alt))) + 
   geom_point(aes(alpha = significant), size = 2) + 
-  facet_wrap(~flank_dist_chr, ncol  = 1, scales = "free") + 
+  facet_wrap(~trait, ncol  = 1, scales = "free") + 
   scale_x_continuous(breaks = xbreaks) + labs(x = "Chromossome", y = "-log(p value)") + 
   guides(colour = FALSE) + geom_vline(data = p_values_sig, aes(xintercept = snp), color = "lightgrey") + 
   geom_hline(yintercept = -log10(thresholds[1,3]), size = 0.8, color = "grey", linetype = "dashed") + 
   theme(legend.position = "none") + scale_alpha_discrete(range = c(0.25, 1)) + scale_color_manual(values = c("black", "tomato3")))
-save_plot("./data/TalkStuff/growth_manhattan.png", p1, base_height = 7, base_aspect_ratio = 1.5)
+
+#save_plot("./data/TalkStuff/growth_manhattan.png", p1, base_height = 7, base_aspect_ratio = 1.5)
 
 significantMarkerList = alply(significantMarkerMatrix, 1, makeMarkerList)
 significant_marker_term = paste(significantMarkerList, collapse = " + ")
